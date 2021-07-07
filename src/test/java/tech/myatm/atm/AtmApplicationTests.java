5489b29c1bc1faeee657ec8add1172e04da7d397
SELVAM DURAI <selvamdurai@selvams-mbp.home>
2021-07-07 15:49:51 +0100

    initial commit

---
+++ src/test/java/tech/myatm/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.myatm.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
