2c065807316d8fce7deae901bac159aae249e09e
SELVAM DURAI <selvamdurai@selvams-mbp.home>
2021-07-09 23:44:57 +0100

    added API Guide

---
+++ src/test/java/com/atm/service/AccountServiceTest.java
@@ -0,0 +1,21 @@
+package com.atm.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void checkIfBalanceIsNotLessThanOverdraftLimit() {
+
+    }
+
+    @Test
+    void withDrawAmount() {
+    }
+
+    @Test
+    void deposit() {
+    }
+}
